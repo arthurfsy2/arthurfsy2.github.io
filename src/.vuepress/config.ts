ad8df482ca8ac613b473c48df97dde4937b74cb2
arthurfsy2 <fsyflh@gmail.com>
2023-08-22 21:30:07 +0800

    search

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,8 +1,7 @@
 import { getDirname, path } from "@vuepress/utils";
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
-import { searchProPlugin } from "vuepress-plugin-search-pro";
-import { componentsPlugin } from "vuepress-plugin-components";
+import { docsearchPlugin } from "@vuepress/plugin-docsearch";
 const __dirname = getDirname(import.meta.url);
 
 export default defineUserConfig({
@@ -25,21 +24,50 @@ export default defineUserConfig({
   },  
 
   plugins: [
-    searchProPlugin({
-      // 索引全部内容
-      indexContent: true,
-      // 为分类和标签添加索引
-      customFields: [
-        {
-          getter: (page) => page.frontmatter.category,
-          formatter: "分类：$content",
-        },
-        {
-          getter: (page) => page.frontmatter.tag,
-          formatter: "标签：$content",
-        },
-      ],
-    }),
+    docsearchPlugin({
+      appId: "LA02Z7ZAIR",
+      apiKey: "35fa3509868353e93e6cf96986bded74",
+      indexName: "4a1801",
+      placeholder: "搜索文档",
+      translations: {
+          button: {
+              buttonText: "搜索文档",
+              buttonAriaLabel: "搜索文档",
+          },
+          modal: {
+              searchBox: {
+                  resetButtonTitle: "清除查询条件",
+                  resetButtonAriaLabel: "清除查询条件",
+                  cancelButtonText: "取消",
+                  cancelButtonAriaLabel: "取消",
+              },
+              startScreen: {
+                  recentSearchesTitle: "搜索历史",
+                  noRecentSearchesText: "没有搜索历史",
+                  saveRecentSearchButtonTitle: "保存至搜索历史",
+                  removeRecentSearchButtonTitle: "从搜索历史中移除",
+                  favoriteSearchesTitle: "收藏",
+                  removeFavoriteSearchButtonTitle: "从收藏中移除",
+              },
+              errorScreen: {
+                  titleText: "无法获取结果",
+                  helpText: "你可能需要检查你的网络连接",
+              },
+              footer: {
+                  selectText: "选择",
+                  navigateText: "切换",
+                  closeText: "关闭",
+                  searchByText: "搜索提供者",
+              },
+              noResultsScreen: {
+                  noResultsText: "无法找到相关结果",
+                  suggestedQueryText: "你可以尝试查询",
+                  reportMissingResultsText: "你认为该查询应该有结果？",
+                  reportMissingResultsLinkText: "点击反馈",
+              },
+          },
+      },
+  }),
     
   ],
   head: [
