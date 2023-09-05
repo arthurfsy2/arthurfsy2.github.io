1bf04e01235344bb451797ce3d22134799387343
arthurfsy2 <fsyflh@gmail.com>
2023-09-06 00:37:32 +0800

    Update client.ts

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -1,11 +1,12 @@
 // .vuepress/client.ts
 import { defineClientConfig } from "@vuepress/client";
 import { setupRunningTimeFooter } from "vuepress-theme-hope/presets/footerRunningTime.js";
+
 import 'vuepress-theme-hope/presets/left-blog-info.scss'
 // 为项目主页的特性添加闪光效果
 import 'vuepress-theme-hope/presets/shinning-feature-panel.scss'
 // 为页面图标添加鼠标悬停的跳动效果
-import 'vuepress-theme-hope/presets/bounce-icon.scss'
+//import 'vuepress-theme-hope/presets/bounce-icon.scss'
 
 export default defineClientConfig({
   setup() {
