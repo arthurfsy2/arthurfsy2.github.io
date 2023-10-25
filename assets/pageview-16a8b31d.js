656c970755e8bf024067a06503bf006fe4682f41
arthurfsy2 <129527972+arthurfsy2@users.noreply.github.com>
2023-10-25 02:31:46 +0000

    Deploying to gh-pages from @ arthurfsy2/arthurfsy2.github.io@e67cd2e6071a6a306fe42287c8c097cdf2f8fc2d 🚀

--- assets/pageview-16a8b31d.js
+++
@@ -1,1 +0,0 @@
-import{R as e}from"./app-c61056fe.js";const a=()=>e({serverURL:"https://waline-discuss.vercel.app/"});export{a as updatePageview};
---
+++ assets/pageview-9624c4b7.js
@@ -0,0 +1,1 @@
+import{R as e}from"./app-72f0721f.js";const a=()=>e({serverURL:"https://waline-discuss.vercel.app/"});export{a as updatePageview};
