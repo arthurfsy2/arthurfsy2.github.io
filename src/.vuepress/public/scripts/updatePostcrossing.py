f545d394e6a6bf0abfa3c43e108d6afe8b7319c8
arthurfsy2 <fsyflh@gmail.com>
2023-11-24 23:21:01 +0800

    cc

---
+++ src/.vuepress/public/scripts/updatePostcrossing.py
@@ -0,0 +1,46 @@
+import os
+import requests
+import hashlib
+import shutil
+
+# 获取当前脚本文件所在目录
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# 定义目录路径和输出路径
+postcrossing_path = os.path.normpath(os.path.join(script_dir, "../../../Arthur/Postcrossing")) #本博客对应的是/src/Arthur/Postcrossing目录
+
+
+print(f"postcrossing_path:\n",postcrossing_path)
+
+def downloadMD(type):
+    if type != '信息汇总':
+        url = f"https://raw.gitmirror.com/arthurfsy2/Postcrossing_map_generator/main/gallery/{type}.md"
+    else:
+        url = f"https://raw.gitmirror.com/arthurfsy2/Postcrossing_map_generator/main/output/{type}.md"
+    print("url:",url)
+    response = requests.get(url)
+    content = response.text.encode('utf-8')
+    with open(f"{type}_new.md","w",encoding="utf-8") as f:
+        f.write(response.text)
+    new_file_path = f"{type}_new.md"
+    old_file_path = f"{postcrossing_path}/{type}.md"
+    new_md5 = md5(new_file_path)
+    old_md5 = md5(old_file_path)
+    if new_md5 != old_md5:
+        
+        shutil.move(new_file_path, old_file_path)
+        print(f"已更新{type}.md")
+    else:
+        os.remove(new_file_path)
+        print(f"{type}.md无需更新")
+
+
+def md5(file_path):
+    with open(file_path, 'rb') as file:
+        data = file.read()
+        md5_hash = hashlib.md5(data).hexdigest()
+    return md5_hash
+types=['sent', 'received', 'favourites', 'popular','信息汇总']
+
+for type in types:
+    downloadMD(type)
