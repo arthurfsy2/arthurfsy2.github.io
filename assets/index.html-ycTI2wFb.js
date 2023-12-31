df83f69a460d4ace8b8ab8514e770a07e5ecef62
arthurfsy2 <arthurfsy2@users.noreply.github.com>
2023-12-31 18:20:47 +0000

    Deploying to gh-pages from @ arthurfsy2/arthurfsy2.github.io@f9c4d439d6794afa8a7f42ab82970c43b60b244e 🚀

---
+++ assets/index.html-ycTI2wFb.js
@@ -0,0 +1,1 @@
+import{_ as o,r as t,o as n,c,d as a}from"./app-Sb7twr_s.js";const r={};function _(s,l){const e=t("AutoCatalog");return n(),c("div",null,[a(e)])}const m=o(r,[["render",_],["__file","index.html.vue"]]);export{m as default};
