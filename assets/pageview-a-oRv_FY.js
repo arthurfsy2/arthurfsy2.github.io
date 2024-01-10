433a543a7d0a1d8a607ca246ff5f424434c91ee5
arthurfsy2 <129527972+arthurfsy2@users.noreply.github.com>
2024-01-10 05:35:20 +0000

    Deploying to gh-pages from @ arthurfsy2/arthurfsy2.github.io@0ec144ce9847132a273eafeeb419acf777f6a72d 🚀

---
+++ assets/pageview-a-oRv_FY.js
@@ -0,0 +1,7 @@
+import{M as t}from"./app-RMek5AHN.js";var a={provider:"Waline",dark:'html[data-theme="dark"]',serverURL:"https://waline-discuss.vercel.app"};const n=async()=>{try{const{pageviewCount:e}=await t(()=>import("./app-RMek5AHN.js").then(r=>r.N),__vite__mapDeps([]));return e({serverURL:a.serverURL})}catch{console.error("@waline/client is not installed!");return}};export{n as updatePageview};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = []
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
