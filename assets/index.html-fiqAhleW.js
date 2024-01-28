8675d2ef8f0db1ecb0d71d78c1a8f71e3a898f62
arthurfsy2 <arthurfsy2@users.noreply.github.com>
2024-01-28 14:14:02 +0000

    Deploying to gh-pages from @ arthurfsy2/arthurfsy2.github.io@6b03a1fac8a1cfac35064729f9c039fa082bb8bc 🚀

---
+++ assets/index.html-fiqAhleW.js
@@ -0,0 +1,1 @@
+import{_ as o,r as t,o as n,c,d as a}from"./app-WN9Rr1va.js";const r={};function _(s,l){const e=t("AutoCatalog");return n(),c("div",null,[a(e)])}const m=o(r,[["render",_],["__file","index.html.vue"]]);export{m as default};
