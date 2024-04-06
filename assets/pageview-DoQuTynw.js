aa80e55e92337b319a63f84b548949df4bad75ff
arthurfsy2 <129527972+arthurfsy2@users.noreply.github.com>
2024-04-06 04:21:38 +0000

    Deploying to gh-pages from @ arthurfsy2/arthurfsy2.github.io@cdc09af4980893d91d76de8f9195b5eec2b9088e 🚀

---
+++ assets/pageview-DoQuTynw.js
@@ -0,0 +1,7 @@
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = []
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
+import{f as o,Y as n}from"./app-BgJwT4mw.js";const s=async()=>{try{const{pageviewCount:e}=await o(()=>import("./app-BgJwT4mw.js").then(r=>r.Z),__vite__mapDeps([])),t=n();return e({serverURL:t.serverURL})}catch{console.error("@waline/client is not installed!")}};export{s as updatePageview};
