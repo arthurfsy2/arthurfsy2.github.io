2b2b58d97319c581db0a57ce58e50ab17aec851b
arthurfsy2 <129527972+arthurfsy2@users.noreply.github.com>
2025-11-10 04:01:00 +0000

    Deploying to gh-pages from @ arthurfsy2/arthurfsy2.github.io@a834cff29796392c022c02af2e138e28178e7707 🚀

---
+++ assets/MyIcon-eFDfRiTX.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./MyIcon.vue_vue_type_style_index_0_lang-CKYp3yL4.js";import"./app-CpJKoviR.js";export{m as default};
--- assets/vidstack-DqAw8m9J-C0VvmRdI.js
+++
@@ -1,1 +0,0 @@
-import{i,a as s}from"./app-cO9b2oYN.js";class a{#i;#t;constructor(t){this.#t=t}start(){i(this.#i)&&this.#s()}stop(){s(this.#i)&&window.cancelAnimationFrame(this.#i),this.#i=void 0}#s(){this.#i=window.requestAnimationFrame(()=>{i(this.#i)||(this.#t(),this.#s())})}}export{a as R};
